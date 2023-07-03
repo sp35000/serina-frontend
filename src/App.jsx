c8255a3ed78fa6af5223e6cae59491f32ef6177c
sp35000 <sp35000@yahoo.com.br>
2023-07-03 16:30:57 -0400

    feat: adding states

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 function NoticiaRow({ noticia }) {
   return (
     <tr>
@@ -11,9 +13,17 @@ function NoticiaRow({ noticia }) {
   );
 }
 
-function NoticiaTable({ noticias }) {
+function NoticiaTable({ noticias, filterText }) {
   const rows = [];
   noticias.forEach((noticia) => {
+    if (
+      noticia.title.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    )
+    {
+      return;
+    }
     rows.push(
       <NoticiaRow
         noticia={noticia}
@@ -39,23 +49,41 @@ function NoticiaTable({ noticias }) {
 }
 
 function FilterableNoticiaTable({ noticias }) {
+  const [filterText, setFilterText] = useState('');
   return (
     <div>
       <h1>Serina News</h1>
-      <NoticiaTable noticias={noticias} />
+      <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
+      <NoticiaTable filterText={filterText} noticias={noticias} />
     </div>
   );
 }
 
+function SearchBar({filterText,onFilterTextChange}) {
+  return (
+    <form>
+      <input 
+        type="text" 
+        value={filterText} placeholder="Search..." 
+        onChange={(e) => onFilterTextChange(e.target.value)} />
+    </form>  );
+}
+
+
 const NEWS = [
-  {id: "1", category: "World", title: "Titulo 01", link: "https://google.com", initial_date: "20230101", final_date: "20231231", hashtag: "#cool"},
-  {id: "2", category: "World", title: "Titulo 02", link: "https://google.com", initial_date: "20230102", final_date: "20231231", hashtag: "#cool"},
-  {id: "3", category: "Brasil", title: "Titulo 03", link: "https://google.com", initial_date: "20230103", final_date: "20231231", hashtag: "#cool"},
-  {id: "4", category: "Brasil", title: "Titulo 04", link: "https://google.com", initial_date: "20230104", final_date: "20231231", hashtag: "#cool"},
-  {id: "5", category: "America", title: "Titulo 05", link: "https://google.com", initial_date: "20230105", final_date: "20231231", hashtag: "#cool"}
-];
+  // {id: "1", category: "World", title: "Titulo 01", link: "https://google.com", initial_date: "20230101", final_date: "20231231", hashtag: "#cool"},
+  // {id: "2", category: "World", title: "Titulo 02", link: "https://google.com", initial_date: "20230102", final_date: "20231231", hashtag: "#cool"},
+  // {id: "3", category: "Brasil", title: "Titulo 03", link: "https://google.com", initial_date: "20230103", final_date: "20231231", hashtag: "#cool"},
+  // {id: "4", category: "Brasil", title: "Titulo 04", link: "https://google.com", initial_date: "20230104", final_date: "20231231", hashtag: "#cool"},
+  // {id: "5", category: "America", title: "Titulo 05", link: "https://google.com", initial_date: "20230105", final_date: "20231231", hashtag: "#cool"}
+  {"id":72,"title":"Portugal encerra Golden Visa para compra de im\u00c3\u00b3veis nas regi\u00c3\u00b5es de Lisboa e Porto","category":"Europe","link":"https:\/\/www.infomoney.com.br\/minhas-financas\/portugal-encerra-golden-visa-para-comprar-de-imoveis-nas-regioes-de-lisboa-e-porto\/","initial_date":20200206,"final_date":20250206,"hashtag":"","media":null,"created_at":null,"updated_at":null},
+  {"id":349,"title":"Portugal vive &quot;crise social&quot;, alerta relat\u00c3\u00b3rio do terceiro estado de emerg\u00c3\u00aancia - DN","category":"Europe","link":"https:\/\/www.dn.pt\/dinheiro\/portugal-vive-crise-social-alerta-relatorio-do-terceiro-estado-de-emergencia-12187400.html?","initial_date":20200513,"final_date":20250513,"hashtag":"","media":null,"created_at":null,"updated_at":null},
+  {"id":383,"title":"Fim da quarentena em Portugal esbarra no medo da popula\u00c3\u00a7\u00c3\u00a3o de sair \u00c3\u00a0s ruas","category":"Europe","link":"https:\/\/g1.globo.com\/mundo\/blog\/sandra-cohen\/post\/2020\/05\/19\/fim-da-quarentena-em-portugal-esbarra-no-medo-da-populacao-de-sair-as-ruas.ghtml","initial_date":20200519,"final_date":20250519,"hashtag":"","media":null,"created_at":null,"updated_at":null},
+  {"id":591,"title":"Governo portugu\u00eas anuncia estatiza\u00e7\u00e3o da TAP","category":"Travel","link":"https:\/\/www.melhoresdestinos.com.br\/tap-air-portugal-estatal.html","initial_date":20200703,"final_date":20250703,"hashtag":"","media":"text","created_at":null,"updated_at":null}
+  ];
 
 
 export default function App() {
   return <FilterableNoticiaTable noticias={NEWS} />;
 }
+
