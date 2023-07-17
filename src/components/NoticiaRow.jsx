6102b77651bd51a57f5310fe1b6b8e8a78e1cc4a
sp35000 <sp35000@yahoo.com.br>
2023-07-17 19:31:03 -0400

    fix: news URL adjusted

--- src/components/NoticiaRow.jsx
+++ src/components/NoticiaRow.jsx
@@ -1,7 +1,7 @@
 function NoticiaRow({ noticia }) {
     return (
       // <li>[{noticia.category}-{noticia.initial_date}]&nbsp;<a href="{noticia.link}" target="_blank">{noticia.title}</a></li>
-      <li>[{noticia.category}]&nbsp;<a href="{noticia.link}" target="_blank">{noticia.title}</a></li>
+      <li>[{noticia.category}]&nbsp;<a href={noticia.link} target="_blank">{noticia.title}</a></li>
       );
   }
 
